67068c1164b7d207b2f0165784826717da873a66
Caue Prado <caue.prado0@gmail.com>
2023-12-31 13:04:53 -0300

    feat: create and expose the API platform

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { NestRootModule } from './modules/nest-root/nest-root.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create<NestExpressApplication>(NestRootModule, {
+    bodyParser: true,
+  });
+
+  const configService: ConfigService = app.get<ConfigService>(
+    ConfigService<{ APPLICATION_PORT: number }, true>,
+  );
+
+  const applicationPort =
+    configService?.get<number>('APPLICATION_PORT', 3000, {
+      infer: true,
+    }) || 3000;
+
+  await app.listen(applicationPort);
+  console.log(`Application listening on port ${applicationPort}`);
 }
-bootstrap();
+
+bootstrap().catch((error: Error) => {
+  console.error(error);
+});
