19666f986df054ef262d4406c5ee1c8e75824e14
Caue Prado <caue.prado0@gmail.com>
2023-12-31 16:24:40 -0300

    chore(configs): update developer configs

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,22 @@
 import { Config } from 'jest';
 
 export const jestConfig: Config = {
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
+  rootDir: '.',
+  moduleFileExtensions: ['js', 'ts'],
+  // modulePaths: ['<rootDir>/src'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '^@modules/(.*)$': '<rootDir>/src/modules/$1',
+    '^@test/(.*)$': '<rootDir>/test/$1',
+  },
   testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
-  collectCoverageFrom: ['**/*.(t|j)s'],
+  collectCoverageFrom: ['**/*.ts'],
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
+  setupFiles: ['reflect-metadata'],
 };
 
 export default jestConfig;
