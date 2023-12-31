41a2ccbee36b9260acdfced8797d61e66e1ee781
Caue Prado <caue.prado0@gmail.com>
2023-12-31 13:07:49 -0300

    feat(dev-environment): add docker-compose.yml with MySQL service, .env.example, and jest.config.ts files for local development

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import { Config } from 'jest';
+
+export const jestConfig: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+};
+
+export default jestConfig;
